0d75858f8f9d65f6a5fc517d1be6ff3abad859c5
T Michael <tmclemons87@gmail.com>
2017-12-29 00:32:53 -0500

    building out component card

--- template/components/sideComponentCard/SideComponentCard.js
+++ template/components/sideComponentCard/SideComponentCard.js
@@ -1,12 +1,33 @@
 import React from "react";
 import Scss from "./sideComponentCard.scss";
+import Avatar from 'material-ui/Avatar';
+import { List, ListItem } from 'material-ui/List';
 
 class SideComponentCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        name: "John Doe",
+        url: 'https://assets.materialup.com/uploads/bebad102-7f40-4941-99cd-54366113003e/avatar-08.png',
+        email: 'johndoe@gmail.com'
+      }
+    }
+  }
   render() {
     return (
-      <div>
-        Side Bar Component
-      </div>
+      <List>
+        <ListItem>
+          <Avatar src={this.state.url}/>
+        </ListItem>
+        <ListItem>
+          {this.state.user.name}
+        </ListItem>
+        <ListItem>
+          {this.state.user.email}
+        </ListItem>
+      </List>
     )
   }
 }
