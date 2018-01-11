19863dc221a4b28423a6fc952b0c9c22f04b8ee4
tmclemons <tmichael@appmetrix.com>
2018-01-11 09:29:17 -0500

    quick demo navigation

--- app/public/ballot/components/header/Header.js
+++ app/public/ballot/components/header/Header.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import GetMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
@@ -13,36 +14,43 @@ class Header extends React.Component {
 
   render() {
     return(
-      <MuiThemeProvider>
-        <AppBar
-          className={'header rep-me-logo'}
-          iconElementLeft={<RepMeLogo />}
-          style={
-            {
-              minHeight: '100px',
-              backgroundColor: '#f5f5f5',
-              paddingLeft: '80px',
-              paddingRight: '80px',
+      <div>
+        <div style={{background: 'black'}}>
+          <Link style={{margin: '0 20px', color: 'white'}} to="/vote">Votes</Link>
+          <Link style={{margin: '0 20px', color: 'white'}} to="/resubmit">Resubmit</Link>
+          <Link style={{margin: '0 20px', color: 'white'}} to="/results">Results</Link>
+        </div>
+        <MuiThemeProvider>
+          <AppBar
+            className={'header rep-me-logo'}
+            iconElementLeft={<RepMeLogo />}
+            style={
+              {
+                minHeight: '100px',
+                backgroundColor: '#f5f5f5',
+                paddingLeft: '80px',
+                paddingRight: '80px',
+              }
             }
-          }
-          titleStyle={
-            {
-              color: 'black',
-              whiteSpace: 'normal',
-              textOverflow: 'initial',
-              maxWidth: '200px',
-              lineHeight: '30px',
-              fontSize: '20px',
-              marginTop: '20px',
+            titleStyle={
+              {
+                color: 'black',
+                whiteSpace: 'normal',
+                textOverflow: 'initial',
+                maxWidth: '200px',
+                lineHeight: '30px',
+                fontSize: '20px',
+                marginTop: '20px',
+              }
             }
-          }
-          iconStyleLeft={{
-            display: 'inherit',
-            maxWidth: '80px',
-            minWidth: '300px',
-          }}
-        />
-      </MuiThemeProvider>
+            iconStyleLeft={{
+              display: 'inherit',
+              maxWidth: '80px',
+              minWidth: '300px',
+            }}
+          />
+        </MuiThemeProvider>
+      </div>
     )
   }
 }
